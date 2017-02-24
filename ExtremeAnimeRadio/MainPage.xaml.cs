08bcf882062f6cb26d38225d85c0b829ec3eade2
Nicholas Omann <AlphaCluster@linux.com>
2017-02-23 21:49:07 -0600

    Integrate website and basic play options.

---
+++ ExtremeAnimeRadio/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ExtremeAnimeRadio
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            AudioPlayer.Source = MediaSource.CreateFromUri(new Uri("http://174.37.159.206:8052/stream"));
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
